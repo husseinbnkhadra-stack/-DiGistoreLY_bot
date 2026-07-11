5e15a7e94347d64deebb8f175af648a424440106
husseinbnkhadra-stack <husseinbnkhadra@gmail.com>
2026-07-11 23:36:22 +0200

    main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ API_TOKEN = '8529951592:AAGWQD_kvwKnI2lPWAP1ulKGZe6OtPsYhB0'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# جزء خاص لجعل Render يظن أنه موقع ويب
 async def handle(request):
-    return web.Response(text="البوت يعمل الآن!")
+    return web.Response(text="Bot is running!")
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -19,12 +18,11 @@ if __name__ == '__main__':
     app = web.Application()
     app.router.add_get('/', handle)
     
-    # تشغيل البوت وخدمة الويب معاً
     import asyncio
     loop = asyncio.get_event_loop()
     runner = web.AppRunner(app)
     loop.run_until_complete(runner.setup())
-    site = web.TCPSite(runner, '0.0.0.0', int(os.environ.get('PORT', 8080)))
+    site = web.TCPSite(runner, '0.0.0.0', int(os.environ.get('PORT', 10000)))
     loop.run_until_complete(site.start())
     
     executor.start_polling(dp, skip_updates=True)
